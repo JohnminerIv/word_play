ff7b01ad399c410ecc0ff0fd3d91885b4800a5d4
John Miner <john.douglas.miner.iv@gmail.com>
2019-11-03 15:03:11 -0800

    completed listogram

--- starter_code/listogram.py
+++ starter_code/listogram.py
@@ -20,19 +20,34 @@ class Listogram(list):
     def add_count(self, word, count=1):
         """Increase frequency count of given word by given count amount."""
         # TODO: Increase word frequency by count
+        if self.__contains__(word):
+            self[self._index(word)][1] += 1
+            self.tokens += 1
+        else:
+            self.append([word, 1])
+            self.tokens += 1
+            self.types += 1
 
     def frequency(self, word):
         """Return frequency count of given word, or 0 if word is not found."""
         # TODO: Retrieve word frequency count
+        return self[self._index(word)][1]
 
     def __contains__(self, word):
         """Return boolean indicating if given word is in this histogram."""
         # TODO: Check if word is in this histogram
+        for type in range(len(self)):
+            if word == self[type][0]:
+                return True
+        return False
 
     def _index(self, target):
         """Return the index of entry containing given target word if found in
         this histogram, or None if target word is not found."""
         # TODO: Implement linear search to find index of entry with target word
+        for i in range(len(self)):
+            if target == self[i][0]:
+                return i
 
 
 def print_histogram(word_list):
